492fafd0f84154f365eac2ef332ce05884ab87dc
kilbot <paul@kilbot.com.au>
2019-03-03 18:49:56 +0800

    electron dev

--- packages/desktop/src/index.ts
+++ packages/desktop/src/index.ts
@@ -1,32 +1,31 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron');
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import isDev from 'electron-is-dev';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow: Electron.BrowserWindow;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html');
+  mainWindow.loadURL(
+    isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
+  );
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
+    // mainWindow = null;
+    mainWindow.destroy();
   });
 }
 
@@ -37,7 +36,7 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  // On macOS it is common for applications and their menu bar
+  // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
@@ -45,10 +44,22 @@ app.on('window-all-closed', function() {
 });
 
 app.on('activate', function() {
-  // On macOS it's common to re-create a window in the app when the
+  // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
+  }
+});
+
+// SSL/TSL: this is the self signed certificate support (for development)
+app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+  // On certificate error we disable default behaviour (stop loading the page)
+  // and we then say "it is all fine - true" to the callback
+  if (isDev) {
+    event.preventDefault();
+    callback(true);
+  } else {
+    callback(false);
   }
 });
 
