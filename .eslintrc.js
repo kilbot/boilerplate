1d53beebc69cfc14b5702aaa7bc6cbcf09466cf4
kilbot <paul@kilbot.com.au>
2019-03-04 02:01:02 +0800

    tweak eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    // 'react-app',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['@typescript-eslint', 'react', 'react-native'],
-  rules: {},
+	parser: '@typescript-eslint/parser',
+	extends: ['plugin:@typescript-eslint/recommended', 'react-app', 'plugin:prettier/recommended'],
+	plugins: ['@typescript-eslint', 'react', 'react-native'],
+	rules: {
+		indent: 'off',
+		'@typescript-eslint/indent': ['error', 'tab'],
+	},
 };
