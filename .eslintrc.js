d0357d4de0be30ef8f0b24d722d60fdccf164c5e
kilbot <paul@kilbot.com.au>
2019-03-08 10:50:20 +0800

    react-native 0.59.0-rc.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: ['plugin:@typescript-eslint/recommended', 'react-app', 'plugin:prettier/recommended'],
-	plugins: ['@typescript-eslint', 'react', 'react-native'],
+	plugins: ['@typescript-eslint', 'react', 'react-native', 'react-hooks'],
 	rules: {
 		indent: 'off',
 		'@typescript-eslint/indent': ['error', 'tab'],
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/prefer-interface': 'off',
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
 	},
 };
