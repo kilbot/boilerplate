2f4fae7a303207099cc4da0a0d7c73bb2cd467bc
kilbot <paul@kilbot.com.au>
2019-03-08 11:11:35 +0800

    basic example

--- packages/common/src/app.tsx
+++ packages/common/src/app.tsx
@@ -8,26 +8,13 @@
  * @format
  */
 
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
 const instructions = Platform.select({
 	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
 	android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
-
-interface Props {}
-export default class App extends Component<Props> {
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text style={styles.welcome}>Welcome to React Native!</Text>
-				<Text style={styles.instructions}>To get started, edit common/src/app.tsx</Text>
-				<Text style={styles.instructions}>{instructions}</Text>
-			</View>
-		);
-	}
-}
 
 const styles = StyleSheet.create({
 	container: {
@@ -47,3 +34,20 @@ const styles = StyleSheet.create({
 		marginBottom: 5,
 	},
 });
+
+function App() {
+	// Declare a new state variable, which we'll call "count"
+	const [count, setCount] = useState(0);
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.welcome}>Welcome to React Native!</Text>
+			<Text style={styles.instructions}>To get started, edit common/src/app.tsx</Text>
+			<Text style={styles.instructions}>{instructions}</Text>
+			<Text style={styles.instructions}>You clicked {count} times</Text>
+			<Button title="Click me" onPress={() => setCount(count + 1)} />
+		</View>
+	);
+}
+
+export default App;
