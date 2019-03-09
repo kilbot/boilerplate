8575fef84b59100c8ca364a0efad000a69a982af
kilbot <paul@kilbot.com.au>
2019-03-10 01:26:26 +0800

    update deploy

--- packages/common/src/firebase.ts
+++ packages/common/src/firebase.ts
@@ -9,7 +9,6 @@ const config = {
 	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
 	// messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
-console.log(config);
 
 const app = firebase.initializeApp(config);
 
