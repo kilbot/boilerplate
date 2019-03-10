8ee6d0a590d21e6c9f214b739eed1daf1e62982e
kilbot <paul@kilbot.com.au>
2019-03-11 01:35:40 +0800

    rough working example on android

--- packages/common/src/component.tsx
+++ packages/common/src/component.tsx
@@ -1,18 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useFirebase from './hooks/use-firebase';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import firebase, { auth, provider } from './firebase';
+import { GoogleSignin } from 'react-native-google-signin';
 
 const Component = () => {
-	const firebase = useFirebase();
-	console.log(firebase);
-	useEffect(() => {
-		// const db = firebase.database();
-		// console.log(db);
-	}, [firebase]);
+	// const firebase = useFirebase();
+	const [user, setUser] = useState(null);
+
+	const handleLogin = async () => {
+		try {
+			// add any configuration settings here:
+			await GoogleSignin.configure();
+
+			const data = await GoogleSignin.signIn();
+
+			// create a new firebase credential with the token
+			const credential = firebase.auth.GoogleAuthProvider.credential(
+				data.idToken,
+				// @ts-ignore
+				data.accessToken
+			);
+			// login with credential
+			const firebaseUserCredential = await firebase.auth().signInWithCredential(credential);
+
+			console.warn(JSON.stringify(firebaseUserCredential.user.toJSON()));
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
 	return (
-		<View>
-			<Text>Hello World!</Text>
+		<View
+			style={{
+				alignItems: 'center',
+				justifyContent: 'center',
+				flex: 1,
+			}}
+		>
+			{user ? (
+				<Button title="Logout" onPress={() => setUser(null)} />
+			) : (
+				<Button title="Login" onPress={handleLogin} />
+			)}
 		</View>
 	);
 };
