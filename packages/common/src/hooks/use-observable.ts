6242a464947e0667b7a9a68417569e9c6167f87e
kilbot <paul@kilbot.com.au>
2019-03-17 01:29:24 +0800

    infinite loop

--- packages/common/src/hooks/use-observable.ts
+++ packages/common/src/hooks/use-observable.ts
@@ -1,28 +1,28 @@
 import { Subject, Subscription } from 'rxjs';
 import { useEffect, useMemo, useState } from 'react';
 
-export default function useObservable(observable: any, initial?: any, inputs: any[] = []) {
-	const [state, setState] = useState(initial);
-	const subject = useMemo(() => new Subject(), inputs);
-
-	useEffect(() => {
-		const subscription = new Subscription();
-		subscription.add(subject);
-		subscription.add(subject.pipe(() => observable).subscribe(value => setState(value)));
-		return () => subscription.unsubscribe();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [subject]);
-
-	return state;
-}
-
-// export default function useObservable(observable: any, initial?: any) {
-// 	const [value, setValue] = useState(initial);
+// export default function useObservable(observable: any, initial?: any, inputs: any[] = []) {
+// 	const [state, setState] = useState(initial);
+// 	const subject = useMemo(() => new Subject(), inputs);
 
 // 	useEffect(() => {
-// 		const subscription = observable.subscribe(setValue);
+// 		const subscription = new Subscription();
+// 		subscription.add(subject);
+// 		subscription.add(subject.pipe(() => observable).subscribe(value => setState(value)));
 // 		return () => subscription.unsubscribe();
-// 	}, [observable]);
+// 		// eslint-disable-next-line react-hooks/exhaustive-deps
+// 	}, [subject]);
 
-// 	return value;
+// 	return state;
 // }
+
+export default function useObservable(observable: any, initial?: any) {
+	const [value, setValue] = useState(initial);
+
+	useEffect(() => {
+		const subscription = observable.subscribe(setValue);
+		return () => subscription.unsubscribe();
+	}, [observable]);
+
+	return value;
+}
