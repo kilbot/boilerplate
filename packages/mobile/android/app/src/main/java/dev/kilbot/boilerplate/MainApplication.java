3a11e136bfda9dfe463d609fb6c6ce43b1eca784
kilbot <paul@kilbot.com.au>
2019-07-27 15:49:37 +0300

    update to react-native 0.60

--- packages/mobile/android/app/src/main/java/dev/kilbot/boilerplate/MainApplication.java
+++ packages/mobile/android/app/src/main/java/dev/kilbot/boilerplate/MainApplication.java
@@ -1,14 +1,11 @@
 package dev.kilbot.boilerplate;
 
 import android.app.Application;
-
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -20,11 +17,13 @@ public class MainApplication extends Application implements ReactApplication {
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          return packages;
+        }
 
     @Override
     protected String getJSMainModuleName() {
